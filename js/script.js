37cb8f7dfd837d5401253995af7ac4d937ff44a5
Jairo <jairo.161.ortega@gmail.com>
2023-01-06 18:00:09 -0600

    Navbar estilizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const navbar = document.querySelector('.navbar');
+const check = document.querySelector('#check');
+
+window.addEventListener('scroll',()=>{
+    if (window.scrollY >50) {
+        navbar.classList.add('nav-scrolled');
+    }else if (window.scrollY <=50) {
+        navbar.classList.remove('nav-scrolled');
+    }
+});
+
 function uncheck() {
-    document.getElementById("check").checked = false;
+    check.checked = false;
 }
